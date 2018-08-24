142f44154815b5b49e44a459b9c0750ccad92420
mofogasy <matthias.spicher@gmail.com>
2018-08-24 07:24:12 +0200

    use arrow functions

--- web-workers/src/play_around/worker.js
+++ web-workers/src/play_around/worker.js
@@ -1,5 +1,5 @@
-onmessage = function(event) {
-    console.log('Worker received some message', event.data);
-};
+// The two are equivalent
+// onmessage = (event) => console.log('Worker received some message', event.data);
+addEventListener('message', (event) => console.log('Worker received some message', event.data));
 
 setInterval(() => postMessage(Math.floor(Math.random() * 10000000)), 200);
