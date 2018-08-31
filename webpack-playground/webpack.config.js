86f0f94d2639d6d61c77d01cba28ad99979fcda0
mofogasy <matthias.spicher@gmail.com>
2018-08-31 16:15:26 +0200

    use clean webpack plugin instead of rimraf

--- webpack-playground/webpack.config.js
+++ webpack-playground/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     entry: {
@@ -37,6 +38,7 @@ module.exports = {
         ]
     },
     plugins: [
+        new CleanWebpackPlugin(['dist']),
         new HtmlWebPackPlugin({
             template: "./src/index.html",
             filename: "./index.html",
