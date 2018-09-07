5b7912c36e55195d2e48f92c818290767491ff62
mofogasy <matthias.spicher@gmail.com>
2018-09-07 10:18:38 +0200

    add loader for css files

--- webpack-playground/src/index.js
+++ webpack-playground/src/index.js
@@ -1,9 +1,11 @@
 import _ from 'lodash';
 import someText from './texts/some.txt';
+import './style.css';
 
 function component() {
     let element = document.createElement('div');
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello-class');
     setTimeout(() => console.log('Arrow function call works!'));
     return element;
 }
