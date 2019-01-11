40bac80ff1856aa574f92efbf0e42ae43063a150
mofogasy <matthias.spicher@gmail.com>
2019-01-11 07:54:45 +0100

    Create wrapper component to check styling behaviour

--- component-styles/src/app/app.module.ts
+++ component-styles/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WrapperComponent } from './wrapper/wrapper.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WrapperComponent
   ],
   imports: [
     BrowserModule
