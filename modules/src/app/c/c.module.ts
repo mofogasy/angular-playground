749efa1c44204ecac06e6d8d27108f605c9d257e
mofogasy <matthias.spicher@gmail.com>
2019-03-29 13:26:12 +0100

    create intermediat module for service to remove cyclic dependencies

--- modules/src/app/c/c.module.ts
+++ modules/src/app/c/c.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CComponent } from './c/c.component';
 import { RouterModule } from '@angular/router';
+import { CApiModule } from './c-api.module';
 
 @NgModule({
   declarations: [CComponent],
@@ -10,6 +11,7 @@ import { RouterModule } from '@angular/router';
     RouterModule.forChild([
       {path: '', component: CComponent}
     ]),
+    CApiModule
   ]
 })
 export class CModule { }
--- modules/src/app/c/c.service.ts
+++ modules/src/app/c/c.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@angular/core';
+import { CApiModule } from './c-api.module';
 
 @Injectable({
-  providedIn: 'root'  // will be included in main bundle instead of lazy loaded chunk
+  // providedIn: 'root'  // will be included in main bundle instead of lazy loaded chunk
+
+  // https://www.softwarearchitekt.at/post/2018/05/06/the-new-treeshakable-providers-api-in-angular-why-how-and-cycles.aspx
+  providedIn: CApiModule  // will be included in chunk and not available in other modules
 })
 export class CService {
 
